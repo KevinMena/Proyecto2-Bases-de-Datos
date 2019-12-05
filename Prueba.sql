1ebd5dc6eeb9e8ff8fad140c2f991da4e15f6e71
Kevin Mena <kevin_mena3@hotmail.com>
2019-12-05 16:50:53 -0400

    Subastas

--- Prueba.sql
+++ Prueba.sql
@@ -18,57 +18,57 @@ CREATE DATABASE 13-10869_11-10278 OWNER postgres;
 /*Tablas*/
 
 CREATE TABLE metodo_pago {
-    id_metodo SERIAL PRIMARY KEY,
+    id_metodo INT PRIMARY KEY,
     nombre_metodo VARCHAR(20)
 }
 
 CREATE TABLE usuario {
-    id_usuario SERIAL PRIMARY KEY,
+    id_usuario INT PRIMARY KEY,
     nombre_usuario VARCHAR(30),
     registrado BOOLEAN,
-    id_pago SERIAL,
+    id_pago INT,
 
     FOREIGN KEY (id_pago) REFERENCES metodo_pago (id_metodo)
 }
 
 CREATE TABLE categoria {
-    id_cat SERIAL PRIMARY KEY,
+    id_cat INT PRIMARY KEY,
     nombre_cat VARCHAR(30),
-    cat_padre SERIAL,
+    cat_padre INT,
 
     FOREIGN KEY (cat_padre) REFERENCES categoria (id_cat)
 }
 
 CREATE TABLE producto {
-    id_prod SERIAL PRIMARY KEY,
+    id_prod INT PRIMARY KEY,
     nombre_prod VARCHAR(30),
     desc_prod VARCHAR(50),
     espec_prod VARCHAR(20),
-    id_cat SERIAL,
+    id_cat INT,
 
     FOREIGN KEY (id_cat) REFERENCES categoria (id_cat)
 }
 
 CREATE TABLE subasta {
-    id_subasta SERIAL PRIMARY KEY,
+    id_subasta INT PRIMARY KEY,
     activa BOOLEAN,
-    fecha_ini DATE,
-    fecha_fin DATE,
+    fecha_ini TIMESTAMP,
+    fecha_fin TIMESTAMP,
     precio_base REAL,
     precio_reserva REAL,
     precio_actual REAL,
     bid_subida REAL,
-    id_vendedor SERIAL,
-    prod_vend SERIAL,
+    id_vendedor INT,
+    prod_vend INT,
 
     FOREIGN KEY (id_vendedor) REFERENCES usuario (id_usuario),
     FOREIGN KEY (prod_vend) REFERENCES producto (id_prod)
 }
 
 CREATE TABLE bid {
-    id_bid SERIAL PRIMARY KEY,
-    id_usuario SERIAL,
-    id_subasta SERIAL,
+    id_bid INT PRIMARY KEY,
+    id_usuario INT,
+    id_subasta INT,
     fecha DATE,
     monto REAL,
 
@@ -146,8 +146,8 @@ VALUES (0, 'Franela AEROPOSTALE', 'Franela azul muy comoda de usar', 'Talla M', 
         (29, 'Franela AEROPOSTALE', 'Franela azul muy comoda de usar', 'Talla M', 1);
 
 INSERT INTO subasta (id_subasta, activa, fecha_ini, fecha_fin, precio_base, precio_reserva, precio_actual, bid_subida, id_vendedor, prod_vend)
-VALUES (0, TRUE, '16/08/2012', '26/08/2019', 5.0, 5.0, 5.0, 1.0, 0, 3),
-        (0, TRUE, '16/08/2012', '26/08/2019', 5.0, 5.0, 5.0, 1.0, 0, 3),
-        (0, TRUE, '16/08/2012', '26/08/2019', 5.0, 5.0, 5.0, 1.0, 0, 3),
-        (0, TRUE, '16/08/2012', '26/08/2019', 5.0, 5.0, 5.0, 1.0, 0, 3),
-        (0, TRUE, '16/08/2012', '26/08/2019', 5.0, 5.0, 5.0, 1.0, 0, 3);
+VALUES (0, TRUE, '16/08/2019 10:00:00', '26/08/2019 12:00:00', 5.0, 5.0, 5.0, 0.1, 0, 3),
+        (1, TRUE, '12/08/2019 08:00:00', '20/08/2019 12:00:00', 10.0, 15.0, 25.0, 1.0, 3, 25),
+        (2, TRUE, '02/08/2019 06:00:00', '02/09/2019 12:00:00', 25.0, 30.0, 72.0, 5.0, 7, 17),
+        (3, TRUE, '13/08/2019 12:00:00', '31/08/2019 12:00:00', 200.0, 200.0, 200.0, 20.0, 14, 5),
+        (4, TRUE, '05/08/2019 14:00:00', '10/08/2019 12:00:00', 1.0, 0.5, 3.2, 0.01, 8, 10);
